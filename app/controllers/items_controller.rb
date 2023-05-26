98a9e07ab6bcd220a65403fa5f32d09bbb59b53b
sbmbgoto <syokusyoku6464@gmail.com>
2023-05-26 17:03:19 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_session, only: [:edit]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -21,8 +23,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :expected_shipping_date_id, :price, :introduction, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
+
+
+
